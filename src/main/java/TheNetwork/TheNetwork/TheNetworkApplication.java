f42f8842d2041e127af370217aafdb6b5da398d9
EmyRukundo <39124680+EmyRukundo@users.noreply.github.com>
2022-03-23 16:32:45 +0200

    first commit

---
+++ src/main/java/TheNetwork/TheNetwork/TheNetworkApplication.java
@@ -0,0 +1,13 @@
+package TheNetwork.TheNetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheNetworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/TheNetwork/TheNetwork/TheNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package TheNetwork.TheNetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
